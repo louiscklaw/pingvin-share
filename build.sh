467aa1a10eba46c8bc4146d8a8dba917b396284a
louiscklaw <louiscklaw@users.noreply.github.com>
2025-04-05 02:28:46 +0800

    update bulid.sh,

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-docker compose -f docker-compose.local.yml -f docker-compose.yml build
-docker compose up -d
+# docker compose -f docker-compose.local.yml -f docker-compose.yml build
+# docker compose up -d
+
+docker buildx build --push --tag 192.168.10.61:5000/pingvin-share:latest --platform linux/amd64 .
 
 echo "done"
